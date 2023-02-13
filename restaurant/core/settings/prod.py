419324c640da69d9f8d1ad3b91ce564b3a8bbddd
Cubinec-py <san4os2012@gmail.com>
2023-02-13 21:15:30 +0200

    prod settings

--- restaurant/core/settings/prod.py
+++ restaurant/core/settings/prod.py
@@ -2,16 +2,17 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'DB_NAME': 'django_db',
+        'DB_USER': 'y2xlxdiifbs7jfwxc3z4',
+        'DB_PASSWORD': os.environ.get('DB_PASSWORD'),
+        'DB_HOST': 'eu - west.connect.psdb.cloud',
+        'DB_PORT': '3306',
+        'MYSQL_ATTR_SSL_CA': '/ etc / ssl / cert.pem',
     }
 }
 
@@ -25,8 +26,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+password = os.environ.get('PASSWORD_REDIS')
 CELERY_TIMEZONE = "Europe/Kiev"
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
-CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
-CELERY_BROKER_URL = 'amqp://rabbitmq:5672'
+CELERY_RESULT_BACKEND = f'redis://default:{password}@redis-10048.c250.eu-central-1-1.ec2.cloud.redislabs.com:10048'
+CELERY_BROKER_URL = f'redis://default:{password}@redis-10048.c250.eu-central-1-1.ec2.cloud.redislabs.com:10048'
